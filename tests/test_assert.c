5f64dd3f484e782dd87851c30f53f7d32957248c
Mateo Patino <mateopatinodev@gmail.com>
2026-07-02 16:56:25 -0500

    Modified test fail message for ASSERT_TRUE

--- tests/test_assert.c
+++ tests/test_assert.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 void assert_true_failed(const char *file_name, int line, const char *func, const char *expr) {
-    fprintf(stderr, "'%s' FAILED\n", func);
-    fprintf(stderr, "'%s' in line %i\n", file_name, line);
-    fprintf(stderr, "Expected '%s' to be true, got false\n", expr);
+    fprintf(stderr, ANSI_RED BOLD "TEST FAILED" ANSI_RESET "\n");
+    fprintf(stderr, "Assertion failed: %s\n", expr);
+    fprintf(stderr, "   at %s:%i\n", file_name, line);
+    fprintf(stderr, "   in %s\n", func);
 }
--- tests/test_assert.h
+++ tests/test_assert.h
@@ -1,6 +1,14 @@
 #ifndef TEST_ASSERT_H
 #define TEST_ASSERT_H
 
+/* Colors */
+#define ANSI_RED "\x1b[31m"
+#define ANSI_RESET "\x1b[0m"
+#define BOLD "\x1b[1m"
+
+/* 
+* Prints information about the the test that failed a true assertion.
+*/
 void assert_true_failed(const char *file_name, int line, const char *func, const char *expr); 
 
 #define ASSERT_TRUE(x) \
@@ -9,6 +17,6 @@ void assert_true_failed(const char *file_name, int line, const char *func, const
             assert_true_failed(__FILE__, __LINE__, __func__, #x);  \
             return false; \
         } \
-    } while (0) \
+    } while (0) 
 
 #endif
